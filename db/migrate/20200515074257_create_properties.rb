30f11cb5c84927fc07e0c5d639626852ab38efea
sadaking <sichirigahama.37@gmail.com>
2020-05-17 16:18:06 +0900

    DBカラムのnull: falseを修正

--- db/migrate/20200515074257_create_properties.rb
+++ db/migrate/20200515074257_create_properties.rb
@@ -1,10 +1,10 @@
 class CreateProperties < ActiveRecord::Migration[5.2]
   def change
     create_table :properties do |t|
-      t.string :property_name, null false
-      t.integer :rent, null false
-      t.text :street_address, null false
-      t.integer :age, null false
+      t.string :property_name, null: false
+      t.integer :rent, null: false
+      t.text :street_address, null: false
+      t.integer :age, null: false
       t.text :remark
     end
   end
